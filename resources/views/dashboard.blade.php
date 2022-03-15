10b87e2c3bafa84749ea85feeca2c78132310912
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-03-15 16:57:26 +0100

    wow accesible logout :(

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @include('components.head')
 <h1>dashboard</h1>
 <p>logged in as {{ auth()->user()->name }}</p>
+<p><a href="/logout">Log out</a>, but please don't it's more fun here</p>
 @include('components.helpers.errors')
 @include('components.friends.list')
 
